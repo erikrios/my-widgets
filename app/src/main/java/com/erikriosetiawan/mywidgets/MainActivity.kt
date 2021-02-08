69e03e3ab22604e825ec2bc3387e12154d34bb68
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-08 11:22:57 +0700

    Handle the start and stop job

--- app/src/main/java/com/erikriosetiawan/mywidgets/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mywidgets/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.erikriosetiawan.mywidgets
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.mywidgets.databinding.ActivityMainBinding
 
@@ -8,9 +14,41 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        private const val JOB_ID = 100
+        private const val SCHEDULE_OF_PERIOD = 86000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            btnStart.setOnClickListener { startJob() }
+            btnStop.setOnClickListener { cancelJob() }
+        }
+    }
+
+    private fun startJob() {
+        val mServiceComponent = ComponentName(this, UpdateWidgetService::class.java)
+
+        val builder = JobInfo.Builder(JOB_ID, mServiceComponent)
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            builder.setPeriodic(900000)
+        } else {
+            builder.setPeriodic(SCHEDULE_OF_PERIOD)
+        }
+        val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        jobScheduler.schedule(builder.build())
+
+        Toast.makeText(this, "Job Service started", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelJob() {
+        val tm = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        tm.cancel(JOB_ID)
+        Toast.makeText(this, "Job Service canceled", Toast.LENGTH_SHORT).show()
     }
 }
