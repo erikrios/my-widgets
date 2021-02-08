fc8ab52b811f9923c46a8e9daf4c3c5e558409cb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-08 11:09:25 +0700

    Create UpdateWidgetService.kt

---
+++ app/src/main/java/com/erikriosetiawan/mywidgets/UpdateWidgetService.kt
@@ -0,0 +1,24 @@
+package com.erikriosetiawan.mywidgets
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.widget.RemoteViews
+import com.erikriosetiawan.mywidgets.NumberGenerator.generate
+
+class UpdateWidgetService : JobService() {
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        val manager = AppWidgetManager.getInstance(this)
+        val view = RemoteViews(packageName, R.layout.random_number_widget)
+        val theWidget = ComponentName(this, RandomNumberWidget::class.java)
+        val lastUpdate = "Random ${generate(100)}"
+        view.setTextViewText(R.id.appwidget_text, lastUpdate)
+        manager.updateAppWidget(theWidget, view)
+        jobFinished(params, false)
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean = false
+}
